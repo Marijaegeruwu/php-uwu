3f410ea88a661dc82f9b940400e50f478049543d
Marijaegeruwu <159972578+Marijaegeruwu@users.noreply.github.com>
2024-02-13 19:26:46 -0600

    Add files via upload

---
+++ problema2.php
@@ -0,0 +1,53 @@
+<html>
+<head></head>
+<body>
+<h1>12662. C16</h1>
+<table border 1px>
+ <th>
+   <td>Points</td>
+   <td>16.79</td>
+   <td>Memory limit</td>
+   <td>32 MiB</td>
+ </th>
+ <th>
+    <td>Time limit(case)</td>
+    <td>1s</td>
+    <td>Time limit(total)</td>
+    <td>1m0s</td>
+ </th>
+ <th>
+    <td>Input size limit (bytes)</td>
+    <td>10 KiB</td>
+ </th>
+
+
+</table>
+
+<h2>Descripcion</h2>
+OMIJal se ha encontrado con una variante en el Concurso ACM, 
+pues el numero de programadores que conforman un equipo puede variar, 
+entonces modifica tu programa anterior pues ahora deberás preguntar el numero de 3 integrantes por equipo,
+al final indica cuantos equipos puedenformarse y cuantos programadores quedan fuera
+<h2>Entrada</h2>
+Tres números reales
+
+<h2>Salida</h2>
+Dos números que representen el número de programadores 
+y el número de programadores por equipo
+
+<h2>Ejemplo</h2>
+
+<?php
+#CODIGO PHP 
+$b=5;
+$c=4;
+#operacion
+$m=$b/$c;
+$resi= $b%$c;
+echo "variables", $b, $c, "<br>";
+echo "Equipos formados: ", $m,"<br>";
+echo"Estudiantes sin equipo: ", $resi;
+
+?>
+</body>
+</html>
